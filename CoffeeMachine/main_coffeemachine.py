4013459c6547075b2efa3f48c7ae7056292b4725
mariia <mariia.yukhym@gmail.com>
2022-10-04 20:15:38 -0400

    fix doing_coffe function; change resource logic

--- CoffeeMachine/main_coffeemachine.py
+++ CoffeeMachine/main_coffeemachine.py
@@ -2,19 +2,15 @@ from menu_coffeemachine import MENU, resources
 
 print(resources)
 
-machine_water = resources['water']
-machine_milk = resources['milk']
-machine_coffee = resources['coffee']
-
 def doing_coffe():
     global resources
-    if machine_water>=resources['water']:
-        resources['water']=machine_water-current_drink_water
-        resources['coffee']=machine_coffee-current_drink_coffee
-        resources['milk'] = machine_milk - current_drink_milk
-        return f"Enjoy your {coffee_type}!"
-    else:
-        return "No watter"
+    machine_water = resources['water']
+    machine_milk = resources['milk']
+    machine_coffee = resources['coffee']
+    resources['water'] = machine_water-current_drink_water
+    resources['coffee'] = machine_coffee-current_drink_coffee
+    resources['milk'] = machine_milk - current_drink_milk
+    return f"Enjoy your {coffee_type}!"
 
 
 
@@ -24,7 +20,7 @@ while play:
     current_drink_water = MENU[coffee_type]['ingredients']['water']
     current_drink_coffee = MENU[coffee_type]['ingredients']['coffee']
     current_drink_milk = MENU[coffee_type]['ingredients']['milk']
-    if resources['water'] <= 0 or resources['coffee'] <= 0 or resources['milk'] <= 0:
+    if resources['water'] < current_drink_water or resources['coffee'] < current_drink_coffee or resources['milk'] <= current_drink_milk:
         print("OUPS smth wrong")
     elif coffee_type == "espresso" or coffee_type == "latte" or coffee_type == "cappuccino":
         print(doing_coffe())
