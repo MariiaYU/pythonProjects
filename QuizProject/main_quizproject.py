2d7f84ed7d1a19d21ecc186c4566d72143cf37b8
mariia <mariia.yukhym@gmail.com>
2022-10-11 22:11:02 -0400

    created the full project

--- QuizProject/main_quizproject.py
+++ QuizProject/main_quizproject.py
@@ -1,5 +1,6 @@
 from question_model_quizproject import Question
 from data_quizproject import question_data
+from quiz_brain_quizproject import QuizBrain
 
 question_bank = []
 for each_question in question_data:
@@ -7,6 +8,15 @@ for each_question in question_data:
     question_answer = each_question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
+quiz_brain = QuizBrain(question_bank)
+
+while quiz_brain.still_has_question():
+    quiz_brain.next_question()
+
+print(f"You've completed the Quiz!\n"f"Your final score is: {quiz_brain.score} / {len(question_bank)}")
+
+
 
 
 
