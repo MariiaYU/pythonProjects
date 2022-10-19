eaa9b3cb75bfd7274559df051c0cf0a661c97d7c
mariia <mariia.yukhym@gmail.com>
2022-10-18 20:02:12 -0400

    created second part of Snake game

--- SnakeGame/main_snakegame.py
+++ SnakeGame/main_snakegame.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from snake_snakegame import Snake
+from food_snakegame import Food
+from scoreboard_snakegame import Score
 import time
 
 screen = Screen()
@@ -9,6 +11,9 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Score()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -16,13 +21,27 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
-
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.score_count()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -280:
+        score.game_over()
+        game_is_on = False
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            score.game_over()
+            game_is_on = False
 
 
 
---
+++ SnakeGame/scoreboard_snakegame.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+ALIGN = "center"
+FONT = ("Arial", 15, "normal")
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.number = 0
+        self.hideturtle()
+        self.goto(y=280, x=0)
+        self.color("white")
+        self.write(f"Score is: {self.number}", align=ALIGN, font=FONT)
+
+    def score_count(self):
+        self.number += 1
+        self.clear()
+        self.write(f"Score is: {self.number}", align=ALIGN, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=ALIGN, font=FONT)
+
