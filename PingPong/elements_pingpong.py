14820b6503169473bbfc4d46da88d103970d16fa
mariia <mariia.yukhym@gmail.com>
2022-10-20 19:13:34 -0400

    finished Ping Pong game

--- PingPong/elements_pingpong.py
+++ PingPong/elements_pingpong.py
@@ -29,11 +29,34 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
-
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def wall_bounce(self):
+        self.y_move *= -1
+
+    def paddle_bounce(self):
+        self.x_move *= -1
+
+    def ball_to_another_player(self):
+        self.home()
+        self.x_move *= -1
+
+
+class SeparateLine(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.hideturtle()
+        self.setx(0)
+        self.sety(300)
+        self.goto(0,-300)
+
 
 
--- PingPong/main_pingpong.py
+++ PingPong/main_pingpong.py
@@ -1,5 +1,6 @@
 from turtle import Screen
-from elements_pingpong import Paddle, Ball
+from elements_pingpong import Paddle, Ball, SeparateLine
+from scoreboard_pingpong import ScoreBoard
 import time
 
 screen = Screen()
@@ -16,17 +17,52 @@ paddle_left.paddle_creation(x=-350, y=0)
 
 ball = Ball()
 
+sep_line = SeparateLine()
+
 
 screen.listen()
 screen.onkey(paddle_right.up, "Up")
 screen.onkey(paddle_right.down, "Down")
 screen.onkey(paddle_left.up, "w")
 screen.onkey(paddle_left.down, "s")
+
+score_board = ScoreBoard()
+
+ball_speed = 0.09
+
+
 game_is_on = True
 while game_is_on:
-    time.sleep(0.09)
+    score_board.update_score_board()
+    time.sleep(ball_speed)
     screen.update()
     ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.wall_bounce()
+
+    if ball.distance(paddle_right) < 50 and ball.xcor() > 320 or ball.distance(paddle_left) < 50 and ball.xcor() < -320:
+        ball.paddle_bounce()
+        ball_speed *= 0.9
+    elif ball.xcor() > 450:
+        score_board.l_score += 1
+        ball.ball_to_another_player()
+    elif ball.xcor() < -450:
+        score_board.r_score += 1
+        ball.ball_to_another_player()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
